d5f81bbaaca9791ff53feb70bdef5d5369ace393
SahilD1509 <sdhiman1509@gmail.com>
2025-06-16 11:53:47 +0530

    n

--- class_17_lambdafunctions2/1map.py
+++ class_17_lambdafunctions2/1map.py
@@ -1,12 +1,15 @@
 enames = ["rahul","sonia","priya"]
+#method 1
 def changecase(name):
     return name.upper()
 new_names = list(map(changecase,enames))
 print(new_names)
 
+#method 2 using lambda map function
 print(enames)
 print(list(map(lambda ename: ename.upper(),enames)))
 
+#method 3 using for loop
 new_names = []
 for ename in enames:
       new_names.append(ename.upper())
